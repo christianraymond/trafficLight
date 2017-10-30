23c618b72f6784e2b1e57af8ffcd7d1fb9c76ce5
christianraymond <ombenisn.raymond@gmail.com>
2017-10-30 17:54:49 +0200

    traffic light blinking

--- light.js
+++ light.js
@@ -1,6 +1,5 @@
 var Light = function(id, color) {
-  var number = document.querySelector('.number');
-
+  var clear;
   var elem = document.getElementById(id);
   elem.classList.add('ligh-background');
 
@@ -11,29 +10,54 @@ var Light = function(id, color) {
 
   this.on = function() {
     lightElem.classList.add('on');
-    lightElem.classList.remove('on');
   }
   this.off = function() {
-    lightElem.classList.add('off');
+    lightElem.classList.remove('on');
+    clearInterval(clear)
   }
-
-  var counter = 0;
-  setInterval(function() {
-    number.innerHTML = counter;
-    counter++;
-    if (counter < 15) {
-      lightElem.classList.add('on');
-    } else if (counter > 15 && counter <= 30) {
-      lightElem.classList.add('off')
-    } else {
-      lightElem.classList.add('orange')
-    }
-    if (counter == 50) {
-      counter = 0;
-    }
-  }, 1000)
+  this.blink = function() {
+    clear = setInterval(function() {
+      greenLight.on();
+      redLight.on();
+      orangeLight.on()
+      setTimeout(function() {
+        greenLight.off();
+        redLight.off();
+        orangeLight.off()
+      }, 150)
+    }, 300)
+  }
 }
 
+var number = document.querySelector('.number');
 var greenLight = new Light('light', 'green');
 var redLight = new Light('light', 'red');
-var orangeLight = new Light('light', 'orange');
+var orangeLight = new Light('light', 'caution');
+
+var counter = 0;
+setInterval(function() {
+  number.innerHTML = counter;
+  counter++;
+  if (counter >= 2 && counter < 10) {
+    greenLight.on();
+    redLight.off();
+    orangeLight.off()
+  } else if (counter >= 10 && counter < 20) {
+    redLight.on();
+    greenLight.off();
+    orangeLight.off()
+  } else if (counter >= 20 && counter < 25) {
+    orangeLight.on()
+    greenLight.off();
+    redLight.off();
+  } else if (counter >= 25 && counter < 30) {
+    greenLight.blink()
+    redLight.blink()
+    orangeLight.blink()
+  } else if (counter == 30) {
+    redLight.off();
+    greenLight.off();
+    orangeLight.off();
+    counter = 0;
+  }
+}, 500)
